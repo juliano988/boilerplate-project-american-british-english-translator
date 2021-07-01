b58ba191bd7263950bbca9e90e7e59589b58ef67
Júlio Faria <juliofariaa@gmail.com>
2021-07-01 19:25:31 -0300

    Tarefa 9 OK

--- routes/api.js
+++ routes/api.js
@@ -15,13 +15,19 @@ module.exports = function (app) {
 
       switch (req.body.locale) {
         case 'american-to-british':
-          res.status(200).json({ text: req.body.text, translation: translator.americanToBritishTranslator(req.body.text) });
+          res.status(200).json({
+            text: req.body.text,
+            translation: translator.americanToBritishTranslator(req.body.text) !== req.body.text ? translator.americanToBritishTranslator(req.body.text) : 'Everything looks good to me!'
+          });
           break;
         case 'british-to-american':
-          res.status(200).json({ text: req.body.text, translation: translator.britishToAmericanTranslator(req.body.text) });
+          res.status(200).json({
+            text: req.body.text,
+            translation: translator.britishToAmericanTranslator(req.body.text) !== req.body.text ? translator.britishToAmericanTranslator(req.body.text) : 'Everything looks good to me!'
+          });
           break;
         default:
-          res.status(200).send({ error: 'Invalid value for locale field' })
+          res.status(200).send({ error: 'Invalid value for locale field' });
           break;
       }
     });
