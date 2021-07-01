7bc1265a0d30f3f30521f3e4aa29eaebd7cfb4dc
Júlio Faria <juliofariaa@gmail.com>
2021-07-01 17:29:39 -0300

    Tarefa 2 OK

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,34 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+  americanToBritishTranslator(text) {
+    let translatedText = text.trim();
+
+    for (const americanWord in americanOnly) {
+      translatedText = translatedText.replace(americanWord, '<span class="highlight">'+americanOnly[americanWord]+'</span>');
+    }
+
+    for (const americanSpelling in americanToBritishSpelling) {
+      translatedText = translatedText.replace(americanSpelling, '<span class="highlight">'+americanToBritishSpelling[americanSpelling]+'</span>');
+    }
+
+    for (const americanTitles in americanToBritishTitles) {
+      translatedText = translatedText.replace(americanTitles, '<span class="highlight">'+americanToBritishTitles[americanTitles]+'</span>');
+    }
+
+    return translatedText
+  }
+
+  britishToAmericanTranslator(text) {
+    let translatedText = text.trim();
+
+    for (const britishWord in britishOnly) {
+      translatedText = translatedText.replace(britishWord, '<span class="highlight">'+britishOnly[britishWord]+'</span>');
+    }
+
+    return translatedText
+  }
+
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,20 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      switch (req.body.locale) {
+        case 'american-to-british':
+          res.status(200).json({ text: req.body.text, translation: translator.americanToBritishTranslator(req.body.text) });
+          break;
+        case 'british-to-american':
+          res.status(200).json({ text: req.body.text, translation: translator.britishToAmericanTranslator(req.body.text) });
+          break;
+        default:
+          break;
+      }
     });
 };
